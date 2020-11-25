504bd9a569a1e3b9a1771e808e26f0282f073777
ml-jin <51935034+ml-jin@users.noreply.github.com>
2020-11-25 20:27:09 +0800

    add config properties

--- PRESTO-345/package/scripts/presto_coordinator.py
+++ PRESTO-345/package/scripts/presto_coordinator.py
@@ -99,7 +99,7 @@ class PrestoMaster(Script):
 
         # Logger.info('Creating symbolic links')
         # create_symbolic_link()
-        Execute("/usr/hdp/3.0.1.0-187/presto/presto-server-345/bin/launcher start --pid-file={} --launcher-log-file={} --server-log-file={}".format('coor.pid',params.presto_log_launcher, params.presto_log_server), user='root')
+        Execute("/usr/hdp/3.0.1.0-187/presto/presto-server-345/bin/launcher start --pid-file={} --launcher-log-file={} --server-log-file={} --config='/var/lib/ambari-server/resources/stacks/HDP/3.0/services/PRESTO/configuration/config.properties'".format('coor.pid',params.presto_log_launcher, params.presto_log_server), user='root')
 
         # self.configure(env) # temperary not using
 
@@ -153,7 +153,7 @@ class PrestoMaster(Script):
         # Logger.info('Starting presto yarn session')
         # cmd = get_start_yarn_session_cmd(params.presto_base_dir, params.presto_yarn_session_name, params.job_manager_heap_size, params.task_manager_heap_size, params.slot_count)
         # Execute(cmd, user=params.presto_user)
-        Execute("/usr/hdp/3.0.1.0-187/presto/presto-server-345/bin/launcher start --pid-file={} --launcher-log-file={} --server-log-file={}".format(params.presto_coor_pid_dir + '/coor.pid',params.presto_log_launcher, params.presto_log_server), user='root')
+        Execute("/usr/hdp/3.0.1.0-187/presto/presto-server-345/bin/launcher start --pid-file={} --launcher-log-file={} --server-log-file={} --config='/var/lib/ambari-server/resources/stacks/HDP/3.0/services/PRESTO/configuration/config.properties'".format('coor.pid',params.presto_log_launcher, params.presto_log_server), user='root')
 
     def status(self, env):
         #raise ClientComponentHasNoStatus()
